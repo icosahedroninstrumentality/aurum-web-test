8031973e1f71fb59a65d15fb51ac37baaf2e6865
icosahedroninstrumentality <icosahedroninstrumentality@proton.me>
2026-03-18 09:13:55 +0100

    more gray shades

--- src/colors.ts
+++ src/colors.ts
@@ -3,7 +3,9 @@ import type { RGBA } from "./types";
 export const blank: RGBA = [0, 0, 0, 0];
 
 export const black: RGBA = [0, 0, 0, 1];
+export const darkGray: RGBA = [0.25, 0.25, 0.25, 1];
 export const gray: RGBA = [0.5, 0.5, 0.5, 1];
+export const lightGray: RGBA = [0.75, 0.75, 0.75, 1];
 export const white: RGBA = [1, 1, 1, 1];
 
 export const dark: RGBA = [0.1,0.1,0.1,1];
